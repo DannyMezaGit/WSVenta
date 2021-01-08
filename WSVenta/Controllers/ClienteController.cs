5fba77d63cc6fd13ebb6f3cb60243a1627e1587e
Daniel Meza <danielmeza.4c@gmail.com>
2021-01-07 21:49:08 -0700

    commit inicial

---
+++ WSVenta/Controllers/ClienteController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WSVenta.models;
+
+namespace WSVenta.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (VentaRealContext db = new VentaRealContext())
+            {
+                var lst = db.Clientes.ToList();
+                return Ok(lst);
+            }
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
